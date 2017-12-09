519db0efc4607139605b1331c22cbd7da1a8feb2
walaa111 <walaagamil18@gmail.com>
2017-12-09 14:43:11 +0200

    s

--- series_app_ads/www/js/index.js
+++ series_app_ads/www/js/index.js
@@ -67,8 +67,8 @@ function initAd(){
                     interstitial: 'ca-app-pub-xxxxxxxxxxx/xxxxxxxxxxx'	//PUT ADMOB ADCODE HERE 
                 },
                 android : {
-                    banner: 'ca-app-pub-7251676025279948/1761225381',		//PUT ADMOB ADCODE HERE 
-                    interstitial: 'ca-app-pub-7251676025279948/8490285263'	//PUT ADMOB ADCODE HERE 
+                    banner: 'ca-app-pub-7251676025279948/1059593204',		//PUT ADMOB ADCODE HERE 
+                    interstitial: 'ca-app-pub-7251676025279948/8599048168'	//PUT ADMOB ADCODE HERE 
                 }
             };
             var admobid = ( /(android)/i.test(navigator.userAgent) ) ? ad_units.android : ad_units.ios;
